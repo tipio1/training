4bb8ca17553b3a6381c20f61654234e9f57eafd9
tipio1 <benjamin.rossier@yahoo.fr>
2021-04-12 16:28:16 +0200

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log("hello world");
+
+// A program is like a factory
+
+// Variables
+// A variable is like a box where you can put anything you want into.
+// A variable must me declared (building the box) and assigned (put stuff inside - give it a value)
+
+let a = 1;
+let b;
+b = 2;
+console.log(a);
+console.log(b);
+
+// A variable may be of different types (string, number, boolean, object, null, undefined)
+let hello = "Hello";
+console.log(hello);     // Hello    // variable is a string
+console.log("hello");   // hello    // not a variable
+
+let c = 42;                         // variable is a number
+let d = true;                       // variable is a boolean
+console.log(c, d); 
+
+let e = c + 1;
+console.log(e);
+
+// Conditionnals
+
+let temperature = 10;
+
+if ( condidion ) {
+    execution
+}
+
+// Iterations
+
+// Functions
