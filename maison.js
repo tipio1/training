c81f29091f05097efbb2a7f0fdf0e08d5605e247
tipio1 <benjamin.rossier@yahoo.fr>
2021-04-23 20:57:07 +0200

    correction 'array' to 'object'

--- maison.js
+++ maison.js
@@ -2,7 +2,7 @@ const yaml = require('js-yaml');
 const fs   = require('fs');
 
 const text = fs.readFileSync('maison.yml', 'utf8')
-const maison = yaml.load(text);
-console.log(maison);
-console.log(maison.wcc_house.date_de_construction)
-console.log(maison.wcc_house.address)
+const house = yaml.load(text);
+console.log(house);
+console.log(house.wcc_house.construction_date)
+console.log(house.wcc_house.address)
