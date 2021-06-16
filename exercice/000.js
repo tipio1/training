5874d5112fab273e0f08112160a6c013aace5d1f
tipio1 <benjamin.rossier@yahoo.fr>
2021-06-16 20:07:21 +0200

    // Count from 0 to 10 => FOR

--- exercice/000.js
+++ exercice/000.js
@@ -3,15 +3,27 @@
 // Check if a number is even => IF
 // same as number divisible by 2 => MODULO
     // Operators : + (addition) - (soustraction) * (multiplication) / (division) % (modulo)
-// Count from 0 to 1000 => FOR
+// Count from 0 to 10 => FOR
 
 console.log('hello world 000');
-let i = 276;
 
+// Check if a number is even => IF
+let i = 277;
+    console.log(i);
 if (i % 2 === 0){
-    console.log(true)
+    console.log(true);
+    console.log("even");
 } else {
-    console.log(false)
+    console.log(false);
+    console.log("odd");
 }
 
-console.log(i);
+// Count from 0 to 10 => FOR
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+// Display odd numbers between 0 and 10 => FOR
+for(let i = 1; i <= 10; i+=2){
+    console.log(i + " - odd number");
+}
